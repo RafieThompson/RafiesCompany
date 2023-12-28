e01809d4f01777be402a87a72bbf527a627ce0d1
Rafie Thompson <rafie@raindata.co.uk>
2023-12-28 11:43:40 +0000

    QoL

--- RafiesCompany/Events/EventBrackenCoil.cs
+++ RafiesCompany/Events/EventBrackenCoil.cs
@@ -18,78 +18,107 @@ namespace RafiesCompany.Events
 
     class BrackenCoilEvent : BanditEvent
     {
-        //AnimationCurve oldAnimationCurve;
+        // AnimationCurve oldAnimationCurve; // If u want to change spawnfrequency
+
+        // List to store the original rarities of enemies
         List<int> rarities = new List<int>();
+
+        // Variables to store the original maximum counts of enemies of specific types
         int oldCoilMax;
         int oldFlowerMax;
 
+        // Returns the name of the event
         public override string GetEventName()
         {
             return "You feel a tingle down your spine...";
         }
 
+        // Called when a new level is loaded
         public override void OnLoadNewLevel(ref SelectableLevel newLevel, ModConfig configs)
         {
-            //oldAnimationCurve = newLevel.enemySpawnChanceThroughoutDay;
-            //newLevel.enemySpawnChanceThroughoutDay = new AnimationCurve(new UnityEngine.Keyframe(0, 500f));
-
+            // Loop through all enemies in the new level
             for (int i = 0; i < newLevel.Enemies.Count; i++)
             {
+                // Store the original rarity of each enemy
                 rarities.Add(newLevel.Enemies[i].rarity);
+
+                // Check if the enemy is of type FlowermanAI, change to whatever enemy you need to patch
                 if (newLevel.Enemies[i].enemyType.enemyPrefab.GetComponent<FlowermanAI>() != null)
                 {
+                    // Set the rarity to a specific value 0 = never spawns, 100 = guaranteed
                     newLevel.Enemies[i].rarity = 999;
 
+                    // Store the original maximum count
                     oldFlowerMax = newLevel.Enemies[i].enemyType.MaxCount;
+
+                    // Set the new maximum count based on the configuration
                     newLevel.Enemies[i].enemyType.MaxCount = configs.FlowermanCoilEventFlowermanMax.Value;
                 }
+
+                // Check if the enemy is of type SpringManAI
                 if (newLevel.Enemies[i].enemyType.enemyPrefab.GetComponent<SpringManAI>() != null)
                 {
+                    // Set the rarity to a specific value 0 = never spawns, 100 = guaranteed
                     newLevel.Enemies[i].rarity = 999;
 
+                    // Store the original maximum count
                     oldCoilMax = newLevel.Enemies[i].enemyType.MaxCount;
+
+                    // Set the new maximum count based on the configuration
                     newLevel.Enemies[i].enemyType.MaxCount = configs.FlowermanCoilEventCoilHeadMax.Value;
                 }
             }
         }
 
+        // Called to clean up changes when the level is unloaded or the event is over
         public override void OnLoadNewLevelCleanup(ref SelectableLevel newLevel)
         {
-            //newLevel.enemySpawnChanceThroughoutDay = oldAnimationCurve;
+            // Loop through all enemies in the new level
             for (int i = 0; i < newLevel.Enemies.Count; i++)
             {
+                // Restore the original rarity of each enemy
                 newLevel.Enemies[i].rarity = rarities[i];
+
+                // Check if the enemy is of type FlowermanAI
                 if (newLevel.Enemies[i].enemyType.enemyPrefab.GetComponent<FlowermanAI>() != null)
                 {
+                    // Restore the original maximum count
                     newLevel.Enemies[i].enemyType.MaxCount = oldFlowerMax;
                 }
+
+                // Check if the enemy is of type SpringManAI
                 if (newLevel.Enemies[i].enemyType.enemyPrefab.GetComponent<SpringManAI>() != null)
                 {
+                    // Restore the original maximum count
                     newLevel.Enemies[i].enemyType.MaxCount = oldCoilMax;
                 }
             }
         }
 
+        // Called to check if the event is valid for the current level
         public override bool IsValid(ref SelectableLevel newLevel)
         {
+            // Flags to check if FlowermanAI and SpringManAI are found
             bool flowerManFound = false;
             bool springManFound = false;
+
+            // Loop through all enemies in the new level
             foreach (var enemy in newLevel.Enemies)
             {
+                // Check if the enemy is of type FlowermanAI
                 if (enemy.enemyType.enemyPrefab.GetComponent<FlowermanAI>() != null)
                 {
                     flowerManFound = true;
                 }
+                // Check if the enemy is of type SpringManAI
                 else if (enemy.enemyType.enemyPrefab.GetComponent<SpringManAI>() != null)
                 {
                     springManFound = true;
                 }
             }
-            if (flowerManFound && springManFound)
-            {
-                return true;
-            }
-            return false;
+
+            // Return true if both FlowermanAI and SpringManAI are found, otherwise return false
+            return flowerManFound && springManFound;
         }
     }
 }
--- RafiesCompany/Events/EventCreation.cs
+++ RafiesCompany/Events/EventCreation.cs
@@ -1,4 +1,5 @@
-﻿using RafiesCompany.Other;
+﻿using BepInEx.Logging;
+using RafiesCompany.Other;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,9 +24,25 @@ namespace RafiesCompany.Events
     public class EventCreator
     {
         List<BanditEventCreator> customEventOrder = new List<BanditEventCreator> { new JesterEventCreator(), new JesterEventCreator() };
-
+        private static ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource("RafiesCompany.Events.EventCreation");
         private int customCurrentIndex = 0;
         private int currentIndex = 0;
+
+        private Dictionary<BanditEventCreator, int> eventWeights = new Dictionary<BanditEventCreator, int>
+    {
+        {new BrackenCoilEventCreator(), 5 },
+        { new RandomDeliveryEventCreator(), 10 },
+        { new LittleGirlEventCreator(), 1 },
+        { new HoardingEventCreator(), 10 },
+        { new JesterEventCreator(), 1 },
+        { new JetpackDeliveryCreator(), 4 },
+        { new LandmineEventCreator(), 10 },
+        { new SnareFleaEventCreator(), 15 },
+        { new SpiderEventCreator(), 10 },
+        { new ThumperEventCreator(), 5 },
+        { new ThunderEventCreator(), 10 },
+        { new TurretEventCreator(), 10 },
+    };
 
         public BanditEvent GetRandomEvent()
         {
@@ -37,15 +54,33 @@ namespace RafiesCompany.Events
 
         public BanditEvent GetRandomEventWithWeight(int eventProbability)
         {
+
             Random random = new Random();
-            int randomValue = random.Next(0, 10);
-            if (randomValue < eventProbability)
+            int totalWeight = eventWeights.Values.Sum();
+            int randomValue = random.Next(totalWeight);
+
+            // Adjust randomValue based on eventProbability
+            randomValue -= eventProbability;
+
+            mls.LogInfo($"Total Weight: {totalWeight}");
+            mls.LogInfo($"Adjusted RandomValue: {randomValue + eventProbability}");
+            foreach (var eventWeight in eventWeights)
             {
-                return new EventNone();
+                randomValue -= eventWeight.Value;
+                mls.LogInfo($"Checking {eventWeight.Key.GetType().Name} with Weight {eventWeight.Value}. Remaining RandomValue: {randomValue + eventProbability}");
+
+                if (randomValue <= 0)
+                {
+                    mls.LogInfo($"Selected Event: {eventWeight.Key.GetType().Name}");
+                    return eventWeight.Key.Create();
+                }
             }
 
-            return GetRandomEvent();
+            // If no event is selected, return a default event (EventNone, for example)
+            mls.LogInfo("No Event Selected. Choosing default event (EventNone).");
+            return new EventNone();
         }
+
 
         public BanditEvent GetEventInOrder()
         {
