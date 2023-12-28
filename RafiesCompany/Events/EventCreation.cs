f089e5a766bd9e01195e69ca3f54eeb4d9371c04
Rafie Thompson <rafie@raindata.co.uk>
2023-12-28 23:26:47 +0000

    Modified algorithm for choosing events.

--- RafiesCompany/Events/EventCreation.cs
+++ RafiesCompany/Events/EventCreation.cs
@@ -23,86 +23,28 @@ namespace RafiesCompany.Events
     }
     public class EventCreator
     {
-        List<BanditEventCreator> customEventOrder = new List<BanditEventCreator> { new JesterEventCreator(), new JesterEventCreator() };
+        List<BanditEventCreator> customEventOrder = new List<BanditEventCreator> { };
         private static ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource("RafiesCompany.Events.EventCreation");
-        private int customCurrentIndex = 0;
-        private int currentIndex = 0;
-
-        private Dictionary<BanditEventCreator, int> eventWeights = new Dictionary<BanditEventCreator, int>
-    {
-        {new BrackenCoilEventCreator(), 5 },
-        { new RandomDeliveryEventCreator(), 10 },
-        { new LittleGirlEventCreator(), 1 },
-        { new HoardingEventCreator(), 10 },
-        { new JesterEventCreator(), 1 },
-        { new JetpackDeliveryCreator(), 4 },
-        { new LandmineEventCreator(), 10 },
-        { new SnareFleaEventCreator(), 15 },
-        { new SpiderEventCreator(), 10 },
-        { new ThumperEventCreator(), 5 },
-        { new ThunderEventCreator(), 10 },
-        { new TurretEventCreator(), 10 },
-    };
-
-        public BanditEvent GetRandomEvent()
+        public BanditEvent GetRandomEventWithFixedChance(int fixedChance)
         {
-            List<BanditEventCreator> events = BanditEventList.GetEvents();
             Random random = new Random();
-            BanditEventCreator randomEvent = events[random.Next(events.Count)];
-            return randomEvent.Create();
-        }
+            int randomValue = random.Next(100); // Generating a random value between 0 and 99
+            mls.LogInfo($"Random Value: {randomValue}, Fixed Chance: {fixedChance}");
 
-        public BanditEvent GetRandomEventWithWeight(int eventProbability)
-        {
-
-            Random random = new Random();
-            int totalWeight = eventWeights.Values.Sum();
-            int randomValue = random.Next(totalWeight);
-
-            // Adjust randomValue based on eventProbability
-            randomValue -= eventProbability;
-
-            mls.LogInfo($"Total Weight: {totalWeight}");
-            mls.LogInfo($"Adjusted RandomValue: {randomValue + eventProbability}");
-            foreach (var eventWeight in eventWeights)
+            if (randomValue < fixedChance)
             {
-                randomValue -= eventWeight.Value;
-                mls.LogInfo($"Checking {eventWeight.Key.GetType().Name} with Weight {eventWeight.Value}. Remaining RandomValue: {randomValue + eventProbability}");
-
-                if (randomValue <= 0)
-                {
-                    mls.LogInfo($"Selected Event: {eventWeight.Key.GetType().Name}");
-                    return eventWeight.Key.Create();
-                }
+                // If the random value is less than the fixed chance, choose a random event
+                List<BanditEventCreator> events = BanditEventList.GetEvents();
+                BanditEventCreator randomEvent = events[random.Next(events.Count)];
+                mls.LogInfo($"Selected Event: {randomEvent.GetType().Name}");
+                return randomEvent.Create();
             }
 
-            // If no event is selected, return a default event (EventNone, for example)
+            // If the random value is greater than or equal to the fixed chance, return a default event
             mls.LogInfo("No Event Selected. Choosing default event (EventNone).");
             return new EventNone();
         }
 
-
-        public BanditEvent GetEventInOrder()
-        {
-            List<BanditEventCreator> events = BanditEventList.GetEvents();
-            BanditEventCreator currentEvent = events[currentIndex];
-            currentIndex++;
-            if (currentIndex >= events.Count)
-            {
-                currentIndex = 0;
-            }
-            return currentEvent.Create();
-        }
-
-        public BanditEvent GetEventInCustomOrder()
-        {
-            BanditEventCreator currentEvent = customEventOrder[customCurrentIndex];
-            customCurrentIndex++;
-            if (customCurrentIndex >= customEventOrder.Count)
-            {
-                customCurrentIndex = 0;
-            }
-            return currentEvent.Create();
-        }
     }
+
 }
