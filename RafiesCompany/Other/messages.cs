4da31f81781722bf489fd5d60884ec2cc0984cb7
Rafie Thompson <rafie@raindata.co.uk>
2023-12-28 23:28:28 +0000

    Modified messages for new events.

--- RafiesCompany/Other/messages.cs
+++ RafiesCompany/Other/messages.cs
@@ -11,7 +11,11 @@ namespace RafiesCompany.Other
         public static string[] IncreaseMessages = {
             "due to impressive profits from our one man teams.",
             "following a successful increase in great assets.",
-            "thanks to major company redundancies."
+            "thanks to major company redundancies.",
+            "as a result of record profits.",
+            "thanks to your hard work, you're a great asset!",
+
+
         };
 
         public static string[] DecreaseMessages =
@@ -22,7 +26,32 @@ namespace RafiesCompany.Other
             "as a punishment to lazy workers.",
             "as a result of our ongoing health and safety lawsuit.",
             "after team 18754009 caused a mask outbreak on 71 Gordion.",
-            "following an increase in TCP-Inhalant abuse amongst our teams.",
+            "following an increase in TZP-Inhalant abuse amongst our teams.",
+            "as a result of adding pockets to our company suits."
+        };
+    }
+
+    public static class QuotaMessages
+    {
+        public static string[] IncreaseMessages = {
+            "due to impressive profits from our one man teams.",
+            "following a successful increase in great assets.",
+            "thanks to major company redundancies.",
+            "as a result of record profits.",
+            "thanks to your hard work, you're a great asset!",
+
+
+        };
+
+        public static string[] DecreaseMessages =
+        {
+            "due to an increase in medical expenses for our Titan teams.",
+            "because of increased scrutiny from the intergalactic auditing company.",
+            "following a sudden surge in demand for boomboxes.",
+            "as a punishment to lazy workers.",
+            "as a result of our ongoing health and safety lawsuit.",
+            "after team 18754009 caused a mask outbreak on 71 Gordion.",
+            "following an increase in TZP-Inhalant abuse amongst our teams.",
             "as a result of adding pockets to our company suits."
         };
     }
