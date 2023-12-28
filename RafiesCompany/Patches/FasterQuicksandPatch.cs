5a7d7a6da90c7f574db186ea228f1f719c34ee29
Rafie Thompson <rafie@raindata.co.uk>
2023-12-28 23:28:52 +0000

    Quicksand patch benched for now.

--- RafiesCompany/Patches/FasterQuicksandPatch.cs
+++ RafiesCompany/Patches/FasterQuicksandPatch.cs
@@ -1,46 +1,46 @@
-﻿using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BepInEx.Logging;
-using GameNetcodeStuff;
-using UnityEngine;
+﻿//using HarmonyLib;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using BepInEx.Logging;
+//using GameNetcodeStuff;
+//using UnityEngine;
 
-namespace RafiesCompany.Patches
-{
-    [HarmonyPatch(typeof(QuicksandTrigger))]
-    internal class QuicksandTriggerPatch
-    {
-        private static ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource("RafiesCompany.Patches.FasterQuicksandPatch");
-        private static float lastLogTime = 0f;
-        private static float logInterval = 1f;
+//namespace RafiesCompany.Patches
+//{
+//    [HarmonyPatch(typeof(QuicksandTrigger))]
+//    internal class QuicksandTriggerPatch
+//    {
+//        private static ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource("RafiesCompany.Patches.FasterQuicksandPatch");
+//        private static float lastLogTime = 0f;
+//        private static float logInterval = 1f;
 
-        [HarmonyPatch("OnTriggerStay")]
-        [HarmonyPostfix]
-        static void ModifyQuicksandEffects(QuicksandTrigger __instance)
-        {
-            // Access the static fields directly using the class name
-            float movementHinderance = RafiesCompanyBase.modConfig.MovementHinderance.Value;
-            float sinkingSpeedMultiplier = RafiesCompanyBase.modConfig.SinkingSpeedMultiplier.Value;
+//        [HarmonyPatch("OnTriggerStay")]
+//        [HarmonyPostfix]
+//        static void ModifyQuicksandEffects(QuicksandTrigger __instance)
+//        {
+//            // Access the static fields directly using the class name
+//            float movementHinderance = RafiesCompanyBase.modConfig.MovementHinderance.Value;
+//            float sinkingSpeedMultiplier = RafiesCompanyBase.modConfig.SinkingSpeedMultiplier.Value;
 
-            if (__instance.sinkingLocalPlayer)
-            {
-                __instance.movementHinderance = movementHinderance;
-                __instance.sinkingSpeedMultiplier = sinkingSpeedMultiplier;
+//            if (__instance.sinkingLocalPlayer)
+//            {
+//                __instance.movementHinderance = movementHinderance;
+//                __instance.sinkingSpeedMultiplier = sinkingSpeedMultiplier;
 
-                if (Time.time - lastLogTime >= logInterval)
-                {
-                    mls.LogInfo($"MovementHinderance changed to: {__instance.movementHinderance}");
-                    mls.LogInfo($"SinkingSpeedMultiplier changed to: {__instance.sinkingSpeedMultiplier}");
+//                if (Time.time - lastLogTime >= logInterval)
+//                {
+//                    mls.LogInfo($"MovementHinderance changed to: {__instance.movementHinderance}");
+//                    mls.LogInfo($"SinkingSpeedMultiplier changed to: {__instance.sinkingSpeedMultiplier}");
 
-                    lastLogTime = Time.time;
-                }
-            }
-        }
-    }
-}
+//                    lastLogTime = Time.time;
+//                }
+//            }
+//        }
+//    }
+//}
 
 
 
