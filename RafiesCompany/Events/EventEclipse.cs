c7711831c8ca82078fc0964c95f5020003e23bba
Rafie Thompson <rafie@raindata.co.uk>
2024-01-06 09:58:12 +0000

    2 new events added

---
+++ RafiesCompany/Events/EventEclipse.cs
@@ -0,0 +1,51 @@
+﻿using RafiesCompany.Other;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RafiesCompany.Events
+{
+    class EclipseEventCreator : BanditEventCreator
+    {
+        public override BanditEvent Create()
+        {
+            return new EclipseEvent();
+        }
+    }
+
+    class EclipseEvent : BanditEvent
+    {
+        LevelWeatherType oldType;
+        bool oldOverrideWeather;
+        int oldMinScrapCount;
+        int oldMaxScrapCount;
+
+        public override string GetEventName()
+        {
+            return "WEATHER ANOMALY DETECTED.";
+        }
+
+        public override void OnLoadNewLevel(ref SelectableLevel newLevel, ModConfig configs)
+        {
+            oldMinScrapCount = newLevel.minScrap;
+            oldMaxScrapCount = newLevel.maxScrap;
+            oldType = newLevel.overrideWeatherType;
+            oldOverrideWeather = newLevel.overrideWeather;
+
+            newLevel.currentWeather = LevelWeatherType.Eclipsed;
+            newLevel.overrideWeather = false;
+            newLevel.minScrap += 10;
+            newLevel.maxScrap += 15;
+        }
+
+        public override void OnLoadNewLevelCleanup(ref SelectableLevel newLevel)
+        {
+            newLevel.currentWeather = oldType;
+            newLevel.overrideWeather = oldOverrideWeather;
+            newLevel.minScrap = oldMaxScrapCount;
+            newLevel.maxScrap = oldMaxScrapCount;
+        }
+    }
+}
---
+++ RafiesCompany/Events/EventKin.cs
@@ -0,0 +1,56 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using RafiesCompany.Other;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RafiesCompany.Events
+{
+    class KinEventCreator : BanditEventCreator
+    {
+        public override BanditEvent Create()
+        {
+            return new KinEvent();
+        }
+    }
+
+    class KinEvent : BanditEvent
+    {
+        public override string GetEventName()
+        {
+            return "THE COMPANY ADVISES YOU TO STAY TOGETHER.";
+        }
+
+        public override void OnLoadNewLevel(ref SelectableLevel newLevel, ModConfig configs)
+        {
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(PlayerControllerB), "KillPlayerServerRpc")]
+        static void KinPostfix(int playerId, bool spawnBody, Vector3 bodyVelocity, int causeOfDeath, int deathAnimation)
+        {
+            RafiesCompanyBase.mls.LogInfo($"Player killed, kin event is active: ");
+
+            // Access the timeOfDay property from the RafiesCompanyBase
+            var timeOfDay = RafiesCompanyBase.instance.GetComponent<TimeOfDay>();
+
+            // Set ship to leave early when a player is killed
+            timeOfDay.votedShipToLeaveEarlyThisRound = true;
+            timeOfDay.SetShipLeaveEarlyServerRpc();
+
+            // Notify players in the chat about the ship leaving early
+            HUDManager.Instance.AddTextToChatOnServer(
+                "<color=red>A MEMBER OF YOUR TEAM HAS DIED. THE SHIP WILL NOW LEAVE EARLY.</color>");
+        }
+    }
+
+
+
+
+
+
+}
